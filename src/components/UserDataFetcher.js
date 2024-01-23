7256eddcd623905ebe584570a94be6f15588c2ad
hiral-codes <hiralpatel160302@gmail.com>
2024-01-23 15:22:16 +0530

    improved api calls

--- src/components/UserDataFetcher.js
+++ src/components/UserDataFetcher.js
@@ -1,38 +1,37 @@
-// UserDataFetcher.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const UserDataFetcher = ({ onDataFetched, children }) => {
   const [userDataArray, setUserDataArray] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const requests = Array.from({ length: 10 }, (_, index) =>
-        axios.get('https://randomuser.me/api/')
-      );
-  
-      const responses = await Promise.all(requests);
-      const fetchedData = responses.map((response) => {
-        const user = response.data.results[0];
-        return {
-          username: `${user.name.first}_${user.name.last}`.toLowerCase(),
-          userProfileImageUrl: user.picture.large,
-        };
-      });
-  
-      setUserDataArray(fetchedData);
-  
-      if (onDataFetched) {
-        onDataFetched(fetchedData);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const requests = Array.from({ length: 10 }, (_, index) =>
+          axios.get('https://randomuser.me/api/')
+        );
+    
+        const responses = await Promise.all(requests);
+        const fetchedData = responses.map((response) => {
+          const user = response.data.results[0];
+          return {
+            username: `${user.name.first}_${user.name.last}`.toLowerCase(),
+            userProfileImageUrl: user.picture.large,
+          };
+        });
+    
+        setUserDataArray(fetchedData);
+    
+        if (onDataFetched) {
+          onDataFetched(fetchedData);
+        }
+      } catch (error) {
+        console.error('Error fetching data from RandomUser API:', error.message);
       }
-    } catch (error) {
-      console.error('Error fetching data from RandomUser API:', error.message);
-    }
-  };
+    };
   
-  fetchData();
-  
+    fetchData();
+  }, []); // Empty dependency array ensures that the effect runs only once on mount
 
   return children(userDataArray); // Render the children component with the fetched data
 };

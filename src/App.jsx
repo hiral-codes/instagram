327666ca2f7eb8b744e2768f00bb7f19bfec8862
hiral-codes <hiralpatel160302@gmail.com>
2024-01-23 05:37:58 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import './App.css'
-import Content from './components/Content'
-import Sidebar from './components/Sidebar'
+import React, { useState } from 'react';
+import './App.css';
+import Content from './components/Content';
+import Sidebar from './components/Sidebar';
+import igLogo from '../public/instalogo.png'
+import UserDataFetcher from './components/UserDataFetcher';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState([]);
+
+  const handleDataFetched = (data) => {
+    setUserData(data);
+    setLoading(false);
+  };
+
   return (
     <>
-    <div className="flex">
-    <Sidebar/>
-    <Content/>
-    </div>
+      {loading ? (
+        // Loader component while data is being fetched
+        <div className="loader-container">
+          <div className="">
+            <img src={igLogo} alt="" className='w-20 mix-blend-screen'/>
+          </div>
+        </div>
+      ) : (
+        // Render your main content once loading is complete
+        <div className="flex">
+          <Sidebar />
+          <Content userData={userData} />
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
